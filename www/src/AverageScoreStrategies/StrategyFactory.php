421e03dc479e3583f00ca9f7a9483995f4594e59
alexandrtroitsky <troitsky.a@gmail.com>
2020-10-07 09:26:57 +0300

    Add constants for StrategyFactory

--- www/src/AverageScoreStrategies/StrategyFactory.php
+++ www/src/AverageScoreStrategies/StrategyFactory.php
@@ -6,6 +6,12 @@ use App\Repository\AggregatedScoreRepository;
 
 class StrategyFactory
 {
+    const MIN_DAYS_FOR_DAY_AVERAGE_INCLUDING = 1;
+    const MAX_DAYS_FOR_DAY_AVERAGE_INCLUDING = 29;
+    const MIN_DAYS_FOR_WEEK_AVERAGE_INCLUDING = 30;
+    const MAX_DAYS_FOR_WEEK_AVERAGE_INCLUDING = 89;
+    const MIN_DAYS_FOR_MONTH_AVERAGE_INCLUDING = 90;
+
     private AggregatedScoreRepository $aggregatedScoreRepository;
 
     public function __construct(AggregatedScoreRepository $aggregatedScoreRepository)
@@ -15,13 +21,13 @@ class StrategyFactory
 
     public function create(int $days): AverageScoreStrategy
     {
-        if ($days >= 1 && $days <= 29) {
+        if ($days >= self::MIN_DAYS_FOR_DAY_AVERAGE_INCLUDING && $days <= self::MAX_DAYS_FOR_DAY_AVERAGE_INCLUDING) {
             $strategy = new DayAverageScoreStrategy($this->aggregatedScoreRepository);
         }
-        if ($days >= 30 && $days <= 89) {
+        if ($days >= self::MIN_DAYS_FOR_WEEK_AVERAGE_INCLUDING && $days <= self::MAX_DAYS_FOR_WEEK_AVERAGE_INCLUDING) {
             $strategy = new WeekAverageScoreStrategy($this->aggregatedScoreRepository);
         }
-        if ($days >= 90) {
+        if ($days >= self::MIN_DAYS_FOR_MONTH_AVERAGE_INCLUDING) {
             $strategy = new MonthAverageScoreStrategy($this->aggregatedScoreRepository);
         }
 
